e53a039d639bb9224ffbafdf199f22eb443076d7
AsthaKirar <asthap602@gmail.com>
2024-10-04 14:20:42 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 const App = () => {
+    const[tasks, settasks] = useState([]);
   const[title,settitle] = useState("");
   const SubmitHandler =(e) =>{
     e.preventDefault();
@@ -9,8 +10,32 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-    console.log(task,title);
+    const copytasks =[...tasks];
+    copytasks.push(task);
+    settasks(copytasks);
+    settitle("");
+
+    
+    // console.log(task,title);
+
   };
+  const ToggleHandler = (index) => {
+    const copytasks = [...tasks];
+    copytasks[index].completed = !copytasks[index].completed;
+    settasks(copytasks);
+};
+const DeleteHandler = (index) => {
+    if (tasks[index].completed || confirm("Are you sure ?")) {
+        const copytasks = [...tasks];
+        copytasks.splice(index, 1);
+        settasks(copytasks);
+    } else {
+        alert("task not deleted!");
+        return;
+    }
+};
+
+
 
   return (
       <div className=" border-t-2 w-screen h-screen bg-zinc-800 flex  items-center flex-col">
@@ -20,12 +45,15 @@ const App = () => {
                   <p>Keeps doing things</p>
               </div>
               <div className="text-4xl font-extrabold flex justify-center items-center w-[120px] h-[120px] rounded-full bg-orange-600">
-                  1/3
+              {tasks.filter((task) => task.completed).length}/
+                    {tasks.length}
+
               </div>
           </div>
           {/*  */}
           <form onSubmit={SubmitHandler} className="w-[25%] flex justify-between px-5 my-[2%]">
               <input onChange={(e=>settitle(e.target.value))}
+              value = {title}
                   placeholder="write your next task..."
                   className="px-5 py-3 text-yellow-100 outline-none w-[85%] rounded-xl bg-zinc-700 "
                   type="text"
@@ -36,45 +64,55 @@ const App = () => {
           </form>
           {/*  */}
           <ul className="list-none w-[25%] ">
-              <li className="mb-5 flex justify-between items-center border rounded-xl p-5">
-                  <div className="flex items-center">
-                      <div className="mr-4 rounded-full w-[30px] h-[30px] border border-orange-600"></div>
-                      <h1 className="text-2xl font-extrabold text-yellow-100">
-                          Task 1
-                      </h1>
-                  </div>
-                  <div className="flex gap-3 text-2xl text-yellow-100">
-                      <i className="ri-file-edit-line"></i>
-                      <i className="ri-delete-bin-3-line"></i>
-                  </div>
-              </li>
-              <li className="mb-5 flex justify-between items-center border rounded-xl p-5">
-                  <div className="flex items-center">
-                      <div className="mr-4 rounded-full w-[30px] h-[30px] bg-green-400"></div>
-                      <h1 className="line-through text-2xl font-extrabold text-yellow-100">
-                          Task 2
-                      </h1>
-                  </div>
-                  <div className="flex gap-3 text-2xl text-yellow-100">
-                      <i className="ri-file-edit-line"></i>
-                      <i className="ri-delete-bin-3-line"></i>
-                  </div>
-              </li>
-              <li className="mb-5 flex justify-between items-center border rounded-xl p-5">
-                  <div className="flex items-center">
-                      <div className="mr-4 rounded-full w-[30px] h-[30px] border border-orange-600"></div>
-                      <h1 className="text-2xl font-extrabold text-yellow-100">
-                          Task 3
-                      </h1>
-                  </div>
-                  <div className="flex gap-3 text-2xl text-yellow-100">
-                      <i className="ri-file-edit-line"></i>
-                      <i className="ri-delete-bin-3-line"></i>
-                  </div>
-              </li>
+            {tasks.length>0 ? (
+                tasks.map((task,index) => {
+                    return (
+                        <li 
+                        key = {task.id}
+                        className="mb-5 flex justify-between items-center border rounded-xl p-5">
+                        <div className="flex items-center">
+
+                        <div
+                         onClick={() => ToggleHandler(index)}
+                                        className={` ${
+                                            task.completed
+                                                ? "bg-green-400"
+                                                : "border border-orange-600"
+                                        } mr-4 rounded-full w-[30px] h-[30px]`}
+                                    ></div>
+
+                            <h1 className={`${
+                                            task.completed ? "line-through" : ""
+                                        } text-2xl font-extrabold text-yellow-100`}
+                                    >
+                                        {task.title}
+
+                                
+                            </h1>
+                        </div>
+                        <div className="flex gap-3 text-2xl text-yellow-100">
+                            <i className="ri-file-edit-line"></i>
+                            <i onClick={() => DeleteHandler(index)}
+
+                            className="ri-delete-bin-3-line"></i>
+                        </div>
+                    </li>
+                    
+                );
+
+                })
+        
+        ) : (
+            <h1 className="mt-5 text-yellow-100 text-2xl font-extrabold text-center">
+                No Task Found
+            </h1>
+        )}
+
+            
           </ul>
       </div>
   );
 };
 
 export default App;
+
